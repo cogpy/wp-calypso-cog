aeaccdd88e56bd61abd250421d0a0f229709f70c
Ellie Martin <ellie.martin@automattic.com>
2025-11-19 11:44:36 +0000

    Add a field to capture the customer name for the UPI method.

--- client/my-sites/checkout/src/hooks/use-create-payment-methods/index.tsx
+++ client/my-sites/checkout/src/hooks/use-create-payment-methods/index.tsx
@@ -16,6 +16,7 @@ import {
 	createAlipayMethod,
 	createAlipayPaymentMethodStore,
 	createRazorpayMethod,
+	createRazorpayPaymentMethodStore,
 	isValueTruthy,
 	translateCheckoutPaymentMethodToWpcomPaymentMethod,
 	type StoredPaymentMethod,
@@ -374,15 +375,18 @@ function useCreateRazorpay( {
 		razorpayConfiguration &&
 		isEnabled( 'checkout/razorpay' );
 
+	const paymentMethodStore = useMemo( () => createRazorpayPaymentMethodStore(), [] );
+
 	return useMemo( () => {
 		return isRazorpayReady && razorpayConfiguration && cartKey
 			? createRazorpayMethod( {
 					razorpayConfiguration,
 					cartKey,
 					submitButtonContent: <CheckoutSubmitButtonContent />,
+					store: paymentMethodStore,
 			  } )
 			: null;
-	}, [ razorpayConfiguration, isRazorpayReady, cartKey ] );
+	}, [ razorpayConfiguration, isRazorpayReady, cartKey, paymentMethodStore ] );
 }
 
 /**
